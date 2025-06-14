34cefbb357cfacdb1b09c3da482cb48289d04402
MizunoYudi <joaomizunovy@gmail.com>
2025-06-14 14:47:20 -0300

    Criação CategoriaLivroController.ts

---
+++ src/controller/CategoriaLivroController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { CategoriaLivroService } from "../service/CategoriaLivroService";
+
+const categoriaLivroService = new CategoriaLivroService();
+
+export function exibirCategoriaLivro(req: Request, res: Response){
+    try{
+        const categoria_livros = categoriaLivroService.listarCategoriaLivro();
+        res.status(200).json({
+            categoria_livros
+        });
+    } catch(e: any){
+        res.status(400).json({Status: "Error", mensagem: e.message});
+    }
+}
