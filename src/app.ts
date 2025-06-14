dc435b7d74481399d6294053e1608587b9300573
MizunoYudi <joaomizunovy@gmail.com>
2025-06-14 14:55:19 -0300

    Correção em app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ const app = express();
 const PORT = process.env.PORT ?? 3090 
 app.use(express.json());
 
-app.get('library/categorias-livro', exibirCategoriaLivro);
+app.get('/library/categorias-livro', exibirCategoriaLivro);
 
 app.listen(PORT, () => console.log(`API em execução no URL: http://localhost:${PORT}`));
