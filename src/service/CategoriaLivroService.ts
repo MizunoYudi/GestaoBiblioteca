81319f9c2f6c94bc65f61e52e52a709031f6e34a
MizunoYudi <joaomizunovy@gmail.com>
2025-06-14 13:42:27 -0300

    Criação CategoriaLivroService.ts

---
+++ src/service/CategoriaLivroService.ts
@@ -0,0 +1,9 @@
+import { CategoriaLivroRepository } from "../repository/CategoriaLivroRepository";
+
+export class CategoriaLivroService {
+    categoriaRepository = CategoriaLivroRepository.getInstance();
+
+    listarCategoriaLivro(){
+        return this.categoriaRepository.buscarCategorias();
+    }
+}
