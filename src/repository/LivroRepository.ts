e549dc37ac4d226bc38de07c12ca8aaa17c74c79
MizunoYudi <joaomizunovy@gmail.com>
2025-06-14 16:44:01 -0300

    Criação LivroRepository.ts

---
+++ src/repository/LivroRepository.ts
@@ -0,0 +1,50 @@
+import { Livro } from "../model/Livro";
+
+export class LivroRepository {
+    private static instance: LivroRepository;
+    private livroList: Livro[] = [];
+
+    private constructor() { };
+
+    public static getInstance(): LivroRepository {
+        if (!this.instance) {
+            this.instance = new LivroRepository();
+        }
+        return this.instance
+    }
+
+    inserirLivro(livroData: any) {
+        this.livroList.push(livroData);
+    }
+
+    buscarLivros() {
+        return this.livroList;
+    }
+
+    buscarLivroIsbn(isbn: string): Livro {
+        const indice = this.livroList.findIndex(l => l.isbn == isbn);
+        if (indice == -1) {
+            throw new Error("Livro não encontrado");
+        } else {
+            return this.livroList[indice];
+        }
+    }
+
+    alterarLivro(titulo: string, autor: string, editora: string, edicao: string, categoria_id: number, isbn: string): Livro {
+        const livro = this.buscarLivroIsbn(isbn);
+
+        titulo ? livro.titulo = titulo : livro.titulo = livro.titulo;
+        autor ? livro.autor = autor : livro.autor = livro.autor;
+        editora ? livro.editora = editora : livro.editora = livro.editora;
+        edicao ? livro.edicao = edicao : livro.edicao = livro.edicao;
+        categoria_id ? livro.categoria_id = categoria_id : livro.categoria_id = livro.categoria_id;
+
+        return livro;
+    }
+
+    excluirLivro(livro: Livro) {
+        const indice = this.livroList.indexOf(livro);
+        this.livroList.splice(indice);
+    }
+
+}
