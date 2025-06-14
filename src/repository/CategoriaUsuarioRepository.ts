56253f3b136ed681ecddddd39f069acae24e4178
MizunoYudi <joaomizunovy@gmail.com>
2025-06-14 15:22:27 -0300

    Criação CategoriaUsuarioRepository.ts

---
+++ src/repository/CategoriaUsuarioRepository.ts
@@ -0,0 +1,34 @@
+import { CategoriaUsuario } from "../model/CategoriaUsuario";
+
+
+export class CategoriaUsuarioRepository {
+    private static instance: CategoriaUsuarioRepository;
+    private categoriaUsuarioList: CategoriaUsuario[] =
+        [
+            {
+                id: 1,
+                nome: "Professor"
+            },
+            {
+                id: 2,
+                nome: "Aluno"
+            },
+            {
+                id: 3,
+                nome: "Bibliotecário"
+            }
+        ];
+
+    private constructor() { };
+
+    public static getInstance(): CategoriaUsuarioRepository {
+        if (!this.instance) {
+            this.instance = new CategoriaUsuarioRepository();
+        }
+        return this.instance;
+    }
+
+    buscarCategorias() {
+        return this.categoriaUsuarioList;
+    }
+}
