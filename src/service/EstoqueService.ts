5b569474cfdd5e29f32645c945726694691ca624
MizunoYudi <joaomizunovy@gmail.com>
2025-06-21 17:18:48 -0300

    Criação EstoqueService.ts

---
+++ src/service/EstoqueService.ts
@@ -0,0 +1,36 @@
+import { Estoque } from "../model/Estoque";
+import { EstoqueRepository } from "../repository/EstoqueRepository";
+
+export class EstoqueService {
+    estoqueRepository = EstoqueRepository.getInstance();
+
+    cadastrarExemplar(estoqueData: any) {
+        const { id, livro_id, quantidade, quantidade_emprestada } = estoqueData;
+
+        if (!id || !livro_id || !quantidade || !quantidade_emprestada) {
+            throw new Error("Informações incompletas para o cadastro do exemplar");
+        }
+
+        const novoExemplar = new Estoque(id, livro_id, quantidade, quantidade_emprestada, true);
+        this.estoqueRepository.inserirExemplar(novoExemplar);
+    }
+
+    listarExemplares() {
+        const exemplares = this.estoqueRepository.buscarExemplares();
+        return exemplares;
+    }
+
+    filtrarPorId(id: number): Estoque {
+        const exemplar = this.estoqueRepository.filtrarPorId(id);
+        return exemplar;
+    }
+
+    atualizarDisponibilidade(disponivel: boolean, id: number) {
+        const exemplar = this.estoqueRepository.alterarExemplar(disponivel, id);
+        return exemplar;
+    }
+
+    removerExemplar(id: number) {
+        this.estoqueRepository.excluirExemplar(id);
+    }
+}
