6b33e1d67215ebd7adeece4568f79c779e591e8c
MizunoYudi <joaomizunovy@gmail.com>
2025-06-22 16:04:32 -0300

    Criação EmprestimoController.ts

---
+++ src/controller/EmprestimoController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { EmprestimoService } from "../service/EmprestimoService";
+
+const emprestimoService = new EmprestimoService();
+
+export class EmprestimoController{
+    novoEmprestimo(req: Request, res: Response){
+        try{
+            const novoEmprestimo = emprestimoService.cadastrarEmprestimo(req.body);
+            res.status(201).json({
+                mensagem: "Emprestimo cadastrado com sucesso!",
+                emprestimo: novoEmprestimo
+            })
+        } catch (e:any) {
+            res.status(400).json({ Status: "Error", mensagem: e.message });
+        }
+    }
+
+    exibirEmprestimos(req: Request, res: Response){
+        try{
+            const emprestimos = emprestimoService.listarEmprestimos();
+            res.status(200).json({
+                emprestimos
+            })
+        } catch (e:any) {
+            res.status(400).json({ Status: "Error", mensagem: e.message });
+        }
+    }
+
+    atualizarEmprestimo(req: Request, res: Response){
+        try{
+            const id = parseInt(req.params.id);
+            emprestimoService.atualizarEmprestimo(id);
+            res.status(200).json({
+                mensagem: "Emprestimo retornado!"
+            })
+        } catch (e:any) {
+            res.status(400).json({ Status: "Error", mensagem: e.message });
+        }
+    }
+}
