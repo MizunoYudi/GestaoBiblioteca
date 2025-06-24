2e4d5e6c7d1175a5c1386e1cf7d8f5c7a53c9237
MizunoYudi <joaomizunovy@gmail.com>
2025-06-24 01:58:21 -0300

    Atualização em EmprestimoRepository.ts

--- src/repository/EmprestimoRepository.ts
+++ src/repository/EmprestimoRepository.ts
@@ -1,4 +1,5 @@
 import { Emprestimo } from "../model/Emprestimo";
+import { Usuario } from "../model/Usuario";
 
 export class EmprestimoRepository {
     private static instance: EmprestimoRepository;
@@ -15,7 +16,7 @@ export class EmprestimoRepository {
 
     inserirEmprestimo(emprestimo: Emprestimo) {
         const id = this.emprestimoList.findIndex(e => e.id == emprestimo.id);
-        if(id == -1){
+        if (id == -1) {
             this.emprestimoList.push(emprestimo);
         } else {
             throw new Error("Já existe um emprestimo com o mesmo id no sistema");
@@ -26,17 +27,35 @@ export class EmprestimoRepository {
         return this.emprestimoList;
     }
 
-    buscarEmprestimoId(id: number){
+    buscarEmprestimoId(id: number) {
         const indice = this.emprestimoList.findIndex(e => e.id == id);
-        if(indice == -1){
+        if (indice == -1) {
             throw new Error("Emprestimo não encontrado");
         }
         return this.emprestimoList[indice];
     }
 
-    registrarDevolucao(entrega: Date, atraso: number, suspensao: Date, id: number){
+    buscarUsuarioEmprestimo(usuario_id: number) {
+        const usuario = this.emprestimoList.filter(e => e.usuario_id == usuario_id);
+        if (usuario.length == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    buscarEstoqueEmprestimo(estoque_id: number) {
+        const estoque = this.emprestimoList.filter(e => e.estoque_id == estoque_id);
+        if (estoque.length == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    registrarDevolucao(entrega: Date, atraso: number, suspensao: Date, id: number) {
         const emp = this.buscarEmprestimoId(id);
-        
+
         emp.data_entrega = entrega;
         emp.dias_atraso = atraso;
         emp.suspensao_ate = suspensao;
