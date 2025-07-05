42c13e8b50e12581fc8f638ce1ddcc58deee191e
mizunoYudi <joaomizunovy@gmail.com>
2025-07-04 21:45:40 -0300

    Atualização em CursoRepository.ts - Adição da opção de curso para não alunos

--- src/repository/CursoRepository.ts
+++ src/repository/CursoRepository.ts
@@ -15,6 +15,10 @@ export class CursoRepository {
             {
                 id: 3,
                 nome: "Administração"
+            },
+            {
+                id: 4,
+                nome: "Sem curso"
             }
         ];
 
